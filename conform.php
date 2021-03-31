1ff6ffe0542e14ba05a4d11a79978f405e8333c1
ArthurCaballeroDev <70220125+ArthurCaballeroDev@users.noreply.github.com>
2021-03-31 12:09:18 +0200

    Correction of the code in the conform.php file

--- conform.php
+++ conform.php
@@ -30,12 +30,11 @@ if (!defined('_PS_VERSION_')) {
 
 class Conform extends Module
 {
-    protected $config_form = false;
 
     public function __construct()
     {
         $this->name = 'conform';
-        $this->tab = 'emailing';
+        $this->tab = 'front_office_features';
         $this->version = '1.0.0';
         $this->author = 'Ukoo';
         $this->need_instance = 0;
@@ -53,7 +52,7 @@ class Conform extends Module
 
         $this->confirmUninstall = $this->l('Are you sure you want to uninstall the module ?');
 
-        $this->ps_versions_compliancy = array('min' => '1.7', 'max' => _PS_VERSION_);
+
     }
 
     /**
@@ -66,119 +65,64 @@ class Conform extends Module
             return Shop::setContext(Shop::CONTEXT_ALL);
         }
         return parent::install() &&
-            $this->registerHook('backOfficeHeader') &&
-            $this->registerHook('displayFooter');
-            Configuration::updateValue('NAME', '') &&
-            Configuration::updateValue('SURNAME', '') &&
-            Configuration::updateValue('OBJECT', '') &&
-            Configuration::updateValue('MESSAGE', '') &&
-            Configuration::updateValue('SUBMIT', '');
+            $this->registerHook('displayLeftColumn') &&
+            $this->registerHook('displayRightColumn') &&
+            $this->registerHook('header') &&
+            Configuration::updateValue('CONFORM_EMAIL', '');
 
 
     }
 
     public function uninstall()
     {
-        Configuration::deleteByName('CONFORM_LIVE_MODE');
-
-        include(dirname(__FILE__).'/sql/uninstall.php');
 
         return parent::uninstall() &&
-            Configuration::deleteByName('NAME', '') &&
-            Configuration::deleteByName('SURNAME', '') &&
-            Configuration::deleteByName('OBJECT', '') &&
-            Configuration::deleteByName('MESSAGE', '') &&
-            Configuration::deleteByName('SUBMIT', '');
+            Configuration::deleteByName('CONFORM_EMAIL', '');
     }
 
-    /**
-     * Load the configuration form
-     */
     public function getContent()
     {
         $output = null;
         if (Tools::isSubmit('submit' . $this->name)) {
-            $Name = Tools::getValue('NAME');
-            $Surname = Tools::getValue('SURNAME');
-            $Object = Tools::getValue('OBJECT');
-            $Message = Tools::getValue('MESSAGE');
-            $Submit = Tools::getValue('SUBMIT');
-            if (!$Name || empty($Name) || !Validate::isGenericName($Name)) {
+            $email = Tools::getValue('CONFORM_EMAIL');
+            if (!$email || empty($email) || !Validate::isGenericName($email)) {
                 $output .= $this->displayError($this->l('Configuration failed'));
             } else {
-                Configuration::updateValue('NAME', $Name,);
-                Configuration::updateValue('SURNAME', $Surname);
-                Configuration::updateValue('OBJECT', $Object);
-                Configuration::updateValue('MESSAGE', $Message);
-                Configuration::updateValue('SUBMIT', $Submit);
+                Configuration::updateValue('CONFORM_EMAIL', $email);
                 $output .= $this->displayConfirmation($this->l('Update successful'));
             }
         }
+        return $output . $this->displayForm();
 
-        $this->context->smarty->assign('module_dir', $this->_path);
 
-        $output = $this->context->smarty->fetch($this->local_path.'views/templates/admin/configure.tpl');
-
-        return $output.$this->renderForm();
     }
 
-    /**
-     * Create the form that will be displayed in the configuration of your module.
-     */
-    //protected function displayForm()
 
-        public function displayForm()
+    public function displayForm()
     {
         $fields_form = array();
         $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
         $fields_form[0]['form'] = array(
             'legend' => array(
-                'title' => $this->l('Contact Form settings'),
+                'title' => $this->l('Conform settings'),
             ),
             'input' => array(
-                array(
-                    'type' => 'text',
-                    'label' => $this->l('Name'),
-                    'name' => 'NAME',
-                    'size' => 5,
-                    'required' => true
-                ),
-                array(
-                    'type' => 'text',
-                    'label' => $this->l('Surname'),
-                    'name' => 'SURNAME',
-                    'size' => 5,
-                    'required' => true
-                ),
+
                 array(
                     'type' => 'text',
                     'label' => $this->l('E-mail'),
-                    'name' => 'EMAIL',
+                    'name' => 'CONFORM_EMAIL',
                     'size' => 5,
                     'required' => true
                 )
-                array(
-                    'type' => 'text',
-                    'label' => $this->l('Object'),
-                    'name' => 'OBJECT',
-                    'size' => 5,
-                    'required' => true
-                )
-                    array(
-                        'type' => 'textarea',
-                        'label' => $this->l('Message'),
-                        'name' => 'MESSAGE',
-                        'size' => 20,
-                        'required' => true
-                    )
+
             ),
             'submit' => array(
                 'title' => $this->l('Submit'),
                 'class' => 'btn btn-default'
             )
         );
-   
-    
+
         $helper = new HelperForm();
         $helper->module = $this;
         $helper->name_controller = $this->name;
@@ -205,123 +149,18 @@ class Conform extends Module
 
         $helper->tpl_vars = array(
             'fields_value' => array(
-                'NAME' => Configuration::get('NAME'),
-                'SURNAME' => Configuration::get('SURNAME'),
-                'EMAIL' => Configuration::get('EMAIL'),
-                'OBJECT' => Configuration::get('OBJECT'),
-                'MESSAGE' => Configuration::get('MESSAGE')
+                'CONFORM_EMAIL' => Configuration::get('CONFORM_EMAIL'),
             ),
         );
+
         return $helper->generateForm($fields_form);
     }
 
-    /**
-     * Create the structure of your form.
-     */
-    protected function getConfigForm()
-    {
-        return array(
-            'form' => array(
-                'legend' => array(
-                'title' => $this->l('Settings'),
-                'icon' => 'icon-cogs',
-                ),
-                'input' => array(
-                    array(
-                        'type' => 'switch',
-                        'label' => $this->l('Live mode'),
-                        'name' => 'CONFORM_LIVE_MODE',
-                        'is_bool' => true,
-                        'desc' => $this->l('Use this module in live mode'),
-                        'values' => array(
-                            array(
-                                'id' => 'active_on',
-                                'value' => true,
-                                'label' => $this->l('Enabled')
-                            ),
-                            array(
-                                'id' => 'active_off',
-                                'value' => false,
-                                'label' => $this->l('Disabled')
-                            )
-                        ),
-                    ),
-                    array(
-                        'col' => 3,
-                        'type' => 'text',
-                        'prefix' => '<i class="icon icon-envelope"></i>',
-                        'desc' => $this->l('Enter a valid email address'),
-                        'name' => 'CONFORM_ACCOUNT_EMAIL',
-                        'label' => $this->l('Email'),
-                    ),
-                    array(
-                        'type' => 'password',
-                        'name' => 'CONFORM_ACCOUNT_PASSWORD',
-                        'label' => $this->l('Password'),
-                    ),
-                ),
-                'submit' => array(
-                    'title' => $this->l('Save'),
-                ),
-            ),
-        );
-    }
-
-    /**
-     * Set values for the inputs.
-     */
-    protected function getConfigFormValues()
-    {
-        return array(
-            'CONFORM_LIVE_MODE' => Configuration::get('CONFORM_LIVE_MODE', true),
-            'CONFORM_ACCOUNT_EMAIL' => Configuration::get('CONFORM_ACCOUNT_EMAIL', 'contact@prestashop.com'),
-            'CONFORM_ACCOUNT_PASSWORD' => Configuration::get('CONFORM_ACCOUNT_PASSWORD', null),
-        );
-    }
-
-    /**
-     * Save form data.
-     */
-    protected function postProcess()
-    {
-        $form_values = $this->getConfigFormValues();
-
-        foreach (array_keys($form_values) as $key) {
-            Configuration::updateValue($key, Tools::getValue($key));
-        }
-    }
-
-    /**
-    * Add the CSS & JavaScript files you want to be loaded in the BO.
-    */
-    public function hookBackOfficeHeader()
-    {
-        if (Tools::getValue('module_name') == $this->name) {
-            $this->context->controller->addJS($this->_path.'views/js/back.js');
-            $this->context->controller->addCSS($this->_path.'views/css/back.css');
-        }
-    }
-
-    /**
-     * Add the CSS & JavaScript files you want to be added on the FO.
-     */
-    public function hookHeader()
-    {
-        $this->context->controller->addJS($this->_path.'/views/js/front.js');
-        $this->context->controller->addCSS($this->_path.'/views/css/front.css');
-    }
-public function hookDisplayLeftColumn($params)
-{
-   $this->context->smarty->assign(
-       array(
-           'Name' => Configuration::get('NAME'),
-           'Surname' => Configuration::get('SURNAME'),
-           'E-mail' => Configuration::get('EMAIL'),
-           'Object' => Configuration::get('OBJECT'),
-           'Message' => Configuration::get('MESSAGE')
-       )
-   );
-   return $this->display(__FILE__, 'views/templates/hook/conform.tpl');
 }
 
-}
+
+
+
+
+
+
